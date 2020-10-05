ceb4e28ef8ba087c268b09c3ca2cc8c56bfe512c
Piers Walter <pierswalter3@googlemail.com>
2020-10-05 17:22:16 +0100

    Updates

--- precache-manifest.80f04b45d90d3c9604f1b0b776158bba.js
+++ precache-manifest.80f04b45d90d3c9604f1b0b776158bba.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "a03b423cd5de69639f7e508e4c9bb127",
+    "revision": "a88d9b69c9e553783dcac0324014ed5d",
     "url": "/index.html"
   },
   {
-    "revision": "bdf766aeb64993201f6e",
+    "revision": "96674f3189d07684200e",
     "url": "/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "936eddb06c3834e0f0d6",
-    "url": "/static/js/2.563c2794.chunk.js"
+    "revision": "57bd06f69c5e0cf3c736",
+    "url": "/static/js/2.14e47dff.chunk.js"
   },
   {
     "revision": "0749163b59fbee32225059cb60c18af6",
-    "url": "/static/js/2.563c2794.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.14e47dff.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bdf766aeb64993201f6e",
-    "url": "/static/js/main.ebce63c6.chunk.js"
+    "revision": "96674f3189d07684200e",
+    "url": "/static/js/main.a1f392c8.chunk.js"
   },
   {
     "revision": "67920a2fb62c044d51f4",
