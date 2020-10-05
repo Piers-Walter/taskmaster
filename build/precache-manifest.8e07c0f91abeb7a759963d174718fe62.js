68aade9a555f2a7619aa8bf9eca1aac561ce29ab
Piers Walter <pierswalter3@googlemail.com>
2020-10-05 17:11:03 +0100

    First Push of Taskmaster

---
+++ build/precache-manifest.8e07c0f91abeb7a759963d174718fe62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a03b423cd5de69639f7e508e4c9bb127",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bdf766aeb64993201f6e",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "936eddb06c3834e0f0d6",
+    "url": "/static/js/2.563c2794.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.563c2794.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bdf766aeb64993201f6e",
+    "url": "/static/js/main.ebce63c6.chunk.js"
+  },
+  {
+    "revision": "67920a2fb62c044d51f4",
+    "url": "/static/js/runtime-main.dd3dffab.js"
+  }
+]);
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Button } from '@material-ui/core';
+import React from 'react';
+import ReactDOM from 'react-dom'
+import Home from './components/Home';
+
+function App() {
+  return (
+    <>
+      <Home/>
+    </>
+  );
+}
+
+export default App;
